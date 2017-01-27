9474735f396abad0755a1cdb50132dc9976dfb96
Vlad Ienciu <Vlad Ienciu>
2017-01-28 00:02:15 +0200

    MVC Course 7

---
+++ MVC Course 7/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Course_7
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ActionName("about") ]
+        public ActionResult SomeMethodName()
+        {
+            return View("");
+        }
+
+        public double Calc()
+        {
+            var list = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };            
+            int sum = 0;
+            foreach (int x in list)
+                sum += x;
+            return sum;
+        }
+
+        public double DisplayCalc()
+        {
+            return Calc();
+        }
+
+        [HttpGet]
+        public ActionResult VerbsTest()
+        {
+            return View();
+        }
+
+        [Authorize(Roles ="Admin")]
+        public ActionResult ThisMethodNeedsAuthorization()
+        {
+            return Content("Hello");
+        }
+    }
+}
---
+++ MVC Course 7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_Course_7.MvcApplication" Language="C#" %>
